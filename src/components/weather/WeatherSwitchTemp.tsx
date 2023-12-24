e96596c91f7fc16dccd3683919df4852984b3ea7
Elxan Civishov <elxan.civishov2000@gmail.com>
2023-12-24 23:06:57 +0400

    updated

--- src/components/weather/WeatherSwitchTemp.tsx
+++ src/components/weather/WeatherSwitchTemp.tsx
@@ -34,7 +34,7 @@ const WeatherSwitchTemp: FC = () => {
           className="sr-only peer"
           onChange={(e) => handleChangeTemp(e)}
         />
-        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none  rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all  peer-checked:bg-colorDarkGreen"></div>
+        <div className="w-16 h-6 bg-gray-200 peer-focus:outline-none  rounded-full peer peer-checked:after:translate-x-10 rtl:peer-checked:after:-translate-x-10 peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all  peer-checked:bg-colorDarkGreen"></div>
       </label>
       <span className="text-[30px] font-light  leading-normal text-white">
         °F
